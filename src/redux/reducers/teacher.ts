a26b3b0fcdc33ebc7a49cf7e2d596ad1f6d82b05
Ankush Chavan <ankushchavan0411@gmail.com>
2023-05-16 18:26:18 +0530

    AC | Add data in reducer for teacher

--- src/redux/reducers/teacher.ts
+++ src/redux/reducers/teacher.ts
@@ -6,11 +6,39 @@ import { ITeacher, TeacherAction, TeacherState } from "../../types/types";
 const initialState: TeacherState = {
   teachers: [
     {
-      id: "sfs2dssd1",
+      id: "sfs1dssd1",
       name: "Dr. Amol Shinde",
-      age: 35,
+      age: 33,
       subject: "Computer Science",
       college: "MGM College",
+    },
+    {
+      id: "err4dssd4",
+      name: "Dr. Pooja Patil",
+      age: 34,
+      subject: "Managment and HR",
+      college: "Devgiri College",
+    },
+    {
+      id: "ssd2dssd2",
+      name: "Dr. Vishal Shirsat",
+      age: 36,
+      subject: "Computer Science & IT",
+      college: "Dr.BAMU College",
+    },
+    {
+      id: "dsd3dssd3",
+      name: "Dr. Prakash Ingale",
+      age: 40,
+      subject: "Socail Science",
+      college: "Peoples College",
+    },
+    {
+      id: "err4dssd4",
+      name: "Dr. Simran Maniyar",
+      age: 29,
+      subject: "Commerce and Finance",
+      college: "Chattrapati College",
     },
   ],
 };
